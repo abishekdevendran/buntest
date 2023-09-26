a0bed46eb3a3bda3bd870b72e76c5a11f1899a47
Abishek <abishekdevendran@gmail.com>
2023-09-26 16:37:16 +0530

    Migrate to svelte

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>import "../app.postcss";</script>
+<div class="text-5xl container h-20 flex items-center font-montserrat font-bold fixed z-10 inset-x-0 top-0">VerbaTrack</div>
+<main class="w-full h-[100svh]">
+  <div class="mt-20"/>
+  <slot/>
+</main>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Button from "@/lib/components/ui/button/button.svelte";
+
+  let name ='Abishek';
+</script>
+<h2>Hello {name}</h2>
+<Button on:click={()=>name+='k'}>Stronger</Button>
